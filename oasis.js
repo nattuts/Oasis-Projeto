4159a7e0991f2bde4ce3b7c74dc39a3e50e2ef7a
nattuts <nathaliafloripes1025@gmail.com>
2025-05-08 13:22:09 -0300

    adição de impedimento

--- oasis.js
+++ oasis.js
@@ -12,6 +12,21 @@ const biblio = document.getElementById("biblioteca");
 document.getElementById("biblioteca").addEventListener("click", function(event){
     event.preventDefault();
     lista.style.display = "block";
-})
-;
+});
 
+const formLivro = document.getElementById('formLivro');
+formLivro.addEventListener('submit', function(event){
+    event.preventDefault();
+    alert("");
+});
+
+const formPesquisa = document.getElementById('formPesquisa');
+const inputPesquisa = document.getElementById('inputPesquisa');
+
+formPesquisa.addEventListener('submit', function(event){
+    const pesquisa = inputPesquisa.value.trim();
+    if(pesquisa == ""){
+        event.preventDefault();
+        alert("Você precisa digitar alguma coisa");
+    }
+});
